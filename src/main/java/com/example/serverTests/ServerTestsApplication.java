73811ee5429f5a55609b7a226560047f88f77f38
Liu <xing.e.liu@accenture.com>
2017-11-01 15:09:55 -0400

    added get request test

--- src/main/java/com/example/serverTests/ServerTestsApplication.java
+++ src/main/java/com/example/serverTests/ServerTestsApplication.java
@@ -1,5 +1,7 @@
 package com.example.serverTests;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.ws.rs.client.Client;
@@ -7,6 +9,7 @@ import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.Response;
 import java.util.Map;
 
+import static org.junit.Assert.assertEquals;
 
 @SpringBootApplication
 public class ServerTestsApplication {
@@ -15,6 +18,7 @@ public class ServerTestsApplication {
 	private static final String SUCCESS_RESULT="<result>success</result>";
 	private static final String PASS = "pass";
 	private static final String FAIL = "fail";
+	static Logger log = LoggerFactory.getLogger(ServerTestsApplication.class.getName());
 
 	private void init(){
 		this.client = ClientBuilder.newClient();
@@ -22,32 +26,36 @@ public class ServerTestsApplication {
 
 	public static void main(String[] args){
 		ServerTestsApplication tester = new ServerTestsApplication();
+		log.info("Initializing tester");
 		//initialize the tester
 		tester.init();
 		//test get all users Web Service Method
-		tester.testPost();
+		log.info("Testing get request");
+		tester.testGet();
 	}
 	//Test: Get list of all users
 	//Test: Check if list is not empty
-	private void testPost(){
-//		GenericType<List<User>> list = new GenericType<List<User>>() {};
-//		List<User> users = client
-//				.target(REST_SERVICE_URL)
-//				.request(MediaType.APPLICATION_XML)
-//				.get(list);
-		String result;
+	private void testGet(){
 		Response response = this.client
 				.target(REST_SERVICE_URL)
 				.request()
 				.get();
 
 		if(response == null){
-			result = FAIL;
+			log.error("Test case name: getTest, Result: " + FAIL);
 		} else {
 			Map<String, Object> jsonResponse = response.readEntity(Map.class);
-			System.out.println("Test case name: getTest, Result: " + jsonResponse);
+//			System.out.println("Keys:");
+//			for (String key : jsonResponse.keySet())
+//				System.out.println(key);
+//			System.out.println("Values");
+//			for (Object o : jsonResponse.values()) {
+//				System.out.println(o);
+//			}
+//			System.out.println(jsonResponse.keySet());
+			log.info("Test case name: getTest, Result: " + jsonResponse);
+			assertEquals(jsonResponse.get("firstName"), "Xing");
+			assertEquals(jsonResponse.get("lastName"), "Liu");
 		}
-
-//		System.out.println("Test case name: getTest, Result: " + result );
 	}
 }
