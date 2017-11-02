98e54b2c793b67519d25939b8bebd8a800cd75be
Liu <xing.e.liu@accenture.com>
2017-11-02 00:17:43 -0400

    Fixed bug with logs

--- src/main/java/com/example/serverTests/ServerTestsApplication.java
+++ src/main/java/com/example/serverTests/ServerTestsApplication.java
@@ -1,75 +1,13 @@
 package com.example.serverTests;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.Form;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import java.util.Map;
-
-import static org.junit.Assert.assertEquals;
 
 @SpringBootApplication
 public class ServerTestsApplication {
-	private Client client;
-	private String REST_SERVICE_URL = "http://xtest-leafiest-hubble.cfapps.io/";
-	private static final String FAIL = "fail";
-	static Logger log = LoggerFactory.getLogger(ServerTestsApplication.class.getName());
 
-	private void init(){
-		this.client = ClientBuilder.newClient();
-	}
-
-	public static void main(String[] args){
-		ServerTestsApplication tester = new ServerTestsApplication();
-		log.info("Initializing tester");
-		//initialize the tester
-		tester.init();
-		//test get all users Web Service Method
-		log.info("Testing get request");
-		tester.testGet();
-		log.info("Testing post request");
-		tester.testPost();
-	}
-
-	private void testGet(){
-		Response response = this.client
-				.target(REST_SERVICE_URL+"xing/myhi")
-				.request()
-				.get();
-		if ((response == null) || (response.getStatus() != 200)){
-			log.error("Test case name: getTest, Result: " + FAIL);
-		} else {
-			Map<String, Object> jsonResponse = response.readEntity(Map.class);
-			assertEquals(jsonResponse.get("firstName"), "Xing");
-			assertEquals(jsonResponse.get("lastName"), "Liu");
-			log.info("testGet() passed");
-		}
-	}
-
-	private void testPost() {
-		Form form = new Form();
-		String firstName = "Mayur";
-		String lastName = "Santani";
-		form.param("firstName", firstName);
-		form.param("lastName", lastName);
-		String str = "{\"firstName\":\"Mayur\",\"lastName\":\"Santani\"}";
-		WebTarget webResource = client.target(REST_SERVICE_URL);
-		Response response = webResource.request(MediaType.APPLICATION_JSON)
-				.post(Entity.json(str));
-		if ((response == null) || (response.getStatus() != 200)){
-			log.error("Test case name: getTest, Result: " + FAIL);
-		} else {
-			Map<String, Object> jsonResponse = response.readEntity(Map.class);
-			assertEquals(jsonResponse.get("name"), "Mayur Santani");
-			assertEquals(jsonResponse.get("age"), 22.0);
-			log.info("testPost() passed");
-		}
+	public static void main(String[] args) {
+		SpringApplication.run(ServerTestsApplication.class, args);
 	}
 }
--- src/test/java/com/example/serverTests/ServerTestsApplicationTests.java
+++ src/test/java/com/example/serverTests/ServerTestsApplicationTests.java
@@ -1,16 +1,89 @@
 package com.example.serverTests;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Form;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = ServerTestsApplication.class)
 public class ServerTestsApplicationTests {
 
+	private Client client;
+	private String REST_SERVICE_URL = "http://xtest-leafiest-hubble.cfapps.io/";
+	private static final String FAIL = "fail";
+	static Logger log = LoggerFactory.getLogger(ServerTestsApplication.class.getName());
+
+	@Before
+	public void setup(){
+		log.info("Initializing tester");
+		this.client = ClientBuilder.newClient();
+	}
+
+//	public static void main(String[] args){
+//		ServerTestsApplication tester = new ServerTestsApplication();
+//
+//		//initialize the tester
+//		tester.init();
+//		//test get all users Web Service Method
+//		log.info("Testing get request");
+//		tester.testGet();
+//		log.info("Testing post request");
+//		tester.testPost();
+//	}
+
 	@Test
-	public void contextLoads() {
+	public void testGet(){
+		log.info("Testing get request");
+		Response response = this.client
+				.target(REST_SERVICE_URL+"xing/myhi")
+				.request()
+				.get();
+		if ((response == null) || (response.getStatus() != 200)){
+			log.error("Test case name: getTest, Result: " + FAIL);
+		} else {
+			Map<String, Object> jsonResponse = response.readEntity(Map.class);
+			assertEquals(jsonResponse.get("firstName"), "Xing");
+			assertEquals(jsonResponse.get("lastName"), "Liu");
+			log.info("testGet() passed");
+		}
+	}
+
+	@Test
+	public void testPost() {
+		log.info("Testing post request");
+		Form form = new Form();
+		String firstName = "Mayur";
+		String lastName = "Santani";
+		form.param("firstName", firstName);
+		form.param("lastName", lastName);
+		String str = "{\"firstName\":\"Mayur\",\"lastName\":\"Santani\"}";
+		WebTarget webResource = client.target(REST_SERVICE_URL);
+		Response response = webResource.request(MediaType.APPLICATION_JSON)
+				.post(Entity.json(str));
+		if ((response == null) || (response.getStatus() != 200)){
+			log.error("Test case name: getTest, Result: " + FAIL);
+		} else {
+			Map<String, Object> jsonResponse = response.readEntity(Map.class);
+			assertEquals(jsonResponse.get("name"), "Mayur Santani");
+			assertEquals(jsonResponse.get("age"), 22.0);
+			log.info("testPost() passed");
+		}
 	}
 
 }
