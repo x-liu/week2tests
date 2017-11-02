3856b0d7e4f2e38ca4bbc197e14e323bc0f36c0d
Liu <xing.e.liu@accenture.com>
2017-11-01 23:34:18 -0400

    added both get and post request tests

--- src/main/java/com/example/serverTests/ServerTestsApplication.java
+++ src/main/java/com/example/serverTests/ServerTestsApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Form;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.Map;
 
@@ -14,9 +18,7 @@ import static org.junit.Assert.assertEquals;
 @SpringBootApplication
 public class ServerTestsApplication {
 	private Client client;
-	private String REST_SERVICE_URL = "http://xtest-leafiest-hubble.cfapps.io/xing/myhi";
-	private static final String SUCCESS_RESULT="<result>success</result>";
-	private static final String PASS = "pass";
+	private String REST_SERVICE_URL = "http://xtest-leafiest-hubble.cfapps.io/";
 	private static final String FAIL = "fail";
 	static Logger log = LoggerFactory.getLogger(ServerTestsApplication.class.getName());
 
@@ -32,30 +34,42 @@ public class ServerTestsApplication {
 		//test get all users Web Service Method
 		log.info("Testing get request");
 		tester.testGet();
+		log.info("Testing post request");
+		tester.testPost();
 	}
-	//Test: Get list of all users
-	//Test: Check if list is not empty
+
 	private void testGet(){
 		Response response = this.client
-				.target(REST_SERVICE_URL)
+				.target(REST_SERVICE_URL+"xing/myhi")
 				.request()
 				.get();
-
-		if(response == null){
+		if ((response == null) || (response.getStatus() != 200)){
 			log.error("Test case name: getTest, Result: " + FAIL);
 		} else {
 			Map<String, Object> jsonResponse = response.readEntity(Map.class);
-//			System.out.println("Keys:");
-//			for (String key : jsonResponse.keySet())
-//				System.out.println(key);
-//			System.out.println("Values");
-//			for (Object o : jsonResponse.values()) {
-//				System.out.println(o);
-//			}
-//			System.out.println(jsonResponse.keySet());
-			log.info("Test case name: getTest, Result: " + jsonResponse);
 			assertEquals(jsonResponse.get("firstName"), "Xing");
 			assertEquals(jsonResponse.get("lastName"), "Liu");
+			log.info("testGet() passed");
+		}
+	}
+
+	private void testPost() {
+		Form form = new Form();
+		String firstName = "Mayur";
+		String lastName = "Santani";
+		form.param("firstName", firstName);
+		form.param("lastName", lastName);
+		String str = "{\"firstName\":\"Mayur\",\"lastName\":\"Santani\"}";
+		WebTarget webResource = client.target(REST_SERVICE_URL);
+		Response response = webResource.request(MediaType.APPLICATION_JSON)
+				.post(Entity.json(str));
+		if ((response == null) || (response.getStatus() != 200)){
+			log.error("Test case name: getTest, Result: " + FAIL);
+		} else {
+			Map<String, Object> jsonResponse = response.readEntity(Map.class);
+			assertEquals(jsonResponse.get("name"), "Mayur Santani");
+			assertEquals(jsonResponse.get("age"), 22.0);
+			log.info("testPost() passed");
 		}
 	}
 }
